88446e6ad32b38a1984c9c881a7bb1af56e03c3c
Сергей Никитин <115070040+Sergeynicitin07@users.noreply.github.com>
2026-05-09 15:02:50 +0300

    mul.c

--- sin/src/mul.c
+++ sin/src/mul.c
@@ -1,6 +1,6 @@
 #include "mul.h"
 #include <math.h>
-
+#include "Add.h"
 
 void veltkamp (double x, double *x_high, double *x_low) {
     // в double 64 бита - 1 sign, 53 у мантисса и 10 у порядок
@@ -37,18 +37,17 @@ void dekker (double x, double y, double *r_1, double *r_2) {
 }
 
 
-dd dd_mul (dd a, dd b) {
-    double s, e;
-    dd res;
- 
-    dekker(a.hm, b.hm, &s, &e);
-    e += a.lm * b.hm;
-    e += a.hm * b.lm;
+dd dd_mul(dd a, dd b) {
+    double ph, pl;
 
-    // сложение с упрощением
-    res.hm = s + e;
+    // получаем точное произведение старших частей (106 бит)
+    dekker(a.hm, b.hm, &ph, &pl);
+
+    pl += a.hm * b.lm + a.lm * b.hm;
+
     // компенсация
-    res.lm = e - (res.hm - s) ;
-    return res;
+    double zh, zl;
+    fast2Sum(ph, pl, &zh, &zl);
 
+    return (dd){zh, zl};
 }
