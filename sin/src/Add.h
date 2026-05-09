29d6b8c7e4b5addb1a86f264d1e1ce4e8585ce5d
Сергей Никитин <115070040+Sergeynicitin07@users.noreply.github.com>
2026-05-09 15:04:40 +0300

    Add.h

--- sin/src/Add.h
+++ sin/src/Add.h
@@ -8,10 +8,7 @@ void fast2Sum (double a, double b, double *s, double *t);
 void is2Sum (double a, double b, double *s, double *t);
 
 
-dd dd_add1 (dd a, dd b);
-
-
-dd dd_add2 (dd a, dd b);
+dd dd_add (dd a, dd b);
 
 
 #endif //UNTITLED36_ADD_H
