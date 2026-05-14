4a348d829c0da807d2d0955638c86d0659960bdc
Сергей Никитин <115070040+Sergeynicitin07@users.noreply.github.com>
2026-05-14 17:01:31 +0300

    struct.h

--- sin/src/struct.h
+++ sin/src/struct.h
@@ -11,6 +11,24 @@ typedef struct{
 extern const dd dd_pi;
 
 
+extern const dd dd_pi_2;
+
+
+extern const dd dd_0_5pi;
+
+
+extern const dd factorial_even[];
+
+
+
+// 2/PI разбитый на 4 части
+extern const double inv_pi[];
+
+
+// PI/2 разбитый на 5 частей
+extern const double p[];
+
+
 extern const dd dd_2pi;
 
 
